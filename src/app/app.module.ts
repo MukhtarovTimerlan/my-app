47efe4f97cb980f95386c4455eb87cf54a7cbca5
MukhtarovTimerlan <90184613+MukhtarovTimerlan@users.noreply.github.com>
2023-06-05 05:36:22 +0300

    Sth shows

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -17,6 +17,7 @@ import { LoginFormComponent } from './login-form/login-form.component';
     LoginFormComponent
   ],
   imports: [
+    HttpClientModule,
     BrowserModule,
     AppRoutingModule
   ],
--- src/app/login-form/login-form.component.ts
+++ src/app/login-form/login-form.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from '../auth.service';
 
+
 @Component({
   selector: 'app-login-form',
   templateUrl: './login-form.component.html',
